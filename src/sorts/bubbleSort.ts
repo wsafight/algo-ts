f42a0d993a3811bb5b6b8a22afd29d2630d9fbc8
ShianWang <984292420@qq.com>
2021-02-24 21:25:52 +0800

    refactor: optimization bubbleSort

--- src/sorts/bubbleSort.ts
+++ src/sorts/bubbleSort.ts
@@ -4,10 +4,15 @@ export default function bubbleSort<T>(arr: T[]) {
     return
   }
   for (let i = 0; i < len; i++) {
+    let sorted = true
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        sorted = false
       }
+    }
+    if (sorted) {
+      break
     }
   }
 }
